05901c53cd1f354515d33fb9f681787f5ac37b07
yogesh <>
2025-03-24 17:45:14 +0530

    playwright

---
+++ playwright-automation/tests/features/login.feature
@@ -0,0 +1,17 @@
+Feature: Login
+
+  Background: 
+    Given user open the saucelab url 
+
+  @smoke
+  Scenario Outline: user successfully logs in to saucelab with valid credentials and verify different pages
+    When user login with "testCredential:username" and "testCredential:password" for register user
+    Then user verify swaglabs home page titles for "<username>" user
+    Then user tap on "Product" title
+    Then user verify add to cart on product detail page
+    Then user tap on "<page>" button from product detail page
+    Then user verify checkout on cart page
+
+  Examples:
+      | page  | 
+      | Cart  | 
---
+++ playwright-automation/tests/steps/homeSteps.ts
@@ -0,0 +1,28 @@
+import {HomePage} from '../pageobjects/homePage';
+import {Given, When, Then, setDefaultTimeout} from '@cucumber/cucumber';
+import { pageFixture } from "../../HooksHelper/pageFixture";
+setDefaultTimeout(60*1000*2);
+let homePage = new HomePage(pageFixture.page);
+
+
+
+Then('user verify swaglabs home page titles for {string} user', async function (username) {
+    await homePage.verifyHomePage(username);   
+    });
+
+Then('user tap on {string} title', async function (product) {
+    await homePage.tapOnProductTitle(product);   
+    });
+  
+Then('user verify add to cart on product detail page', async function () {
+    await homePage.verifyAddtoCartBtnOnProductDetailPage();   
+    });
+
+Then('user tap on {string} button from product detail page', async function (cart) {
+    await homePage.tapOnCartBtn(cart);   
+    });
+
+Then('user verify checkout on cart page', async function () {
+    await homePage.verifyCheckoutBtnOncartPage();   
+    });
+    
