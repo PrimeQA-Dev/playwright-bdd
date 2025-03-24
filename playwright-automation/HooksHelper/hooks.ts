8dc10961b079831eee3b7476813017a565d4ef4c
testdummytest <163297472+testdummytest@users.noreply.github.com>
2025-03-24 19:02:01 +0530

    Update hooks.ts

--- playwright-automation/HooksHelper/hooks.ts
+++ playwright-automation/HooksHelper/hooks.ts
@@ -1,32 +1,51 @@
 import { BeforeAll, AfterAll, Before, After, Status } from '@cucumber/cucumber';
 import { Browser, BrowserContext, chromium } from '@playwright/test';
 import { pageFixture } from './pageFixture';
+import { sendEmail } from "../utils/emailSender";
+import * as fs from "fs";
+import * as path from "path";
+
 
 let browser: Browser;
 let context: BrowserContext;
 
 BeforeAll(async () => {
-  console.log(
-    'Launch Browser using Playwright and Chromium browser, performed once, before the start of all test scenarios.',
-  );
+  console.log("Launch Browser before tests...");
   browser = await chromium.launch({ headless: false });
 });
 
 AfterAll(async () => {
-  console.log('Closes Browser before the start of each scenario');
+  console.log("Closing Browser after tests...");
   await browser.close();
+
+  // Prepare attachments
+  const reportPaths = [
+    path.join(__dirname, "../test-results/cucumber-report-smoke.html"),
+  ];
+
+  // Check if reports exist
+  const attachments = reportPaths
+    .filter(report => fs.existsSync(report))
+    .map(report => ({
+      filename: path.basename(report),
+      path: report,
+    }));
+
+  // Send email with reports
+  await sendEmail("Automation Test Report", "Please find the attached test report.", attachments);
 });
 
 Before(async () => {
-  console.log('Launch Browser, performed before each individual test scenario');
+  console.log("Setting up browser context before each test...");
   context = await browser.newContext();
   const page = await context.newPage();
   pageFixture.page = page;
 });
 
 After(async ({ pickle, result }) => {
-  console.log('Browser closed after each scenario, and result?.status');
-  // screenshot
+  console.log("Cleaning up after scenario...");
+
+  // Take screenshot if test failed
   if (result?.status == Status.FAILED) {
     await pageFixture.page.screenshot({
       path: `./test-results/screenshots/${pickle.name}.png`,
