60ae0dca31b50361de4cf1ed8d3cfd66eba6ad32
testdummytest <163297472+testdummytest@users.noreply.github.com>
2025-03-24 18:57:54 +0530

    Create emailSender.ts

---
+++ playwright-automation/utils/emailSender.ts
@@ -0,0 +1,30 @@
+import * as nodemailer from "nodemailer";
+
+export async function sendEmail(subject: string, body: string, attachments: any[] = []) {
+    const SENDER_EMAIL = "automationreport477@gmail.com";
+    const SENDER_PASSWORD = "jepgjwtiwkdblajf";
+    const RECEIVER_EMAIL = "shahyogesh0902@gmail.com";
+
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: SENDER_EMAIL,
+            pass: SENDER_PASSWORD,
+        },
+    });
+
+    const mailOptions: any = {
+        from: SENDER_EMAIL,
+        to: RECEIVER_EMAIL,
+        subject: subject,
+        text: body,
+        attachments: attachments, // Send attachments
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        console.log("Email sent successfully!");
+    } catch (error) {
+        console.error(`Failed to send email: ${error}`);
+    }
+}
